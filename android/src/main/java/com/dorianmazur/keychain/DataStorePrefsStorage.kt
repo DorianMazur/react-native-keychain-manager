d41268349719f6c0cec30cd528a048ae108b5826
MazurDorian <mazur.dorian15@gmail.com>
2024-09-10 11:11:30 +0300

    feat: use android DataStore Preferences

---
+++ android/src/main/java/com/dorianmazur/keychain/DataStorePrefsStorage.kt
@@ -0,0 +1,118 @@
+package com.dorianmazur.keychain
+
+import android.content.Context
+import android.util.Base64
+import androidx.datastore.core.DataMigration
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.SharedPreferencesMigration
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.facebook.react.bridge.ReactApplicationContext
+import com.dorianmazur.keychain.KeychainModule.KnownCiphers
+import com.dorianmazur.keychain.PrefsStorage.KEYCHAIN_DATA
+import com.dorianmazur.keychain.PrefsStorage.ResultSet
+import com.dorianmazur.keychain.PrefsStorage.getKeyForCipherStorage
+import com.dorianmazur.keychain.PrefsStorage.getKeyForPassword
+import com.dorianmazur.keychain.PrefsStorage.getKeyForUsername
+import com.dorianmazur.keychain.PrefsStorage.isKeyForCipherStorage
+import com.dorianmazur.keychain.cipherStorage.CipherStorage
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+
+@Suppress("unused")
+class DataStorePrefsStorage(reactContext: ReactApplicationContext) : PrefsStorage {
+
+  private val Context.prefs: DataStore<Preferences> by preferencesDataStore(
+    name = KEYCHAIN_DATA,
+    produceMigrations = ::sharedPreferencesMigration
+  )
+  private val prefs: DataStore<Preferences> = reactContext.prefs
+  private val prefsData: Preferences get() = callSuspendable { prefs.data.first() }
+
+  private fun sharedPreferencesMigration(context: Context): List<DataMigration<Preferences>> {
+    return listOf(SharedPreferencesMigration(context, KEYCHAIN_DATA))
+  }
+
+  override fun getEncryptedEntry(service: String): ResultSet? {
+    val bytesForUsername = getBytesForUsername(service)
+    val bytesForPassword = getBytesForPassword(service)
+    var cipherStorageName = getCipherStorageName(service)
+
+    // in case of wrong password or username
+    if (bytesForUsername == null || bytesForPassword == null) return null
+    if (cipherStorageName == null) {
+      // If the CipherStorage name is not found, we assume it is because the entry was written by an older
+      // version of this library. The older version used Facebook Conceal, so we default to that.
+      cipherStorageName = KnownCiphers.FB
+    }
+    return ResultSet(cipherStorageName, bytesForUsername, bytesForPassword)
+  }
+
+  override fun removeEntry(service: String) {
+    val keyForUsername = stringPreferencesKey(getKeyForUsername(service))
+    val keyForPassword = stringPreferencesKey(getKeyForPassword(service))
+    val keyForCipherStorage = stringPreferencesKey(getKeyForCipherStorage(service))
+    callSuspendable {
+      prefs.edit {
+        it.remove(keyForUsername)
+        it.remove(keyForPassword)
+        it.remove(keyForCipherStorage)
+      }
+    }
+  }
+
+  override fun storeEncryptedEntry(
+    service: String,
+    encryptionResult: CipherStorage.EncryptionResult,
+  ) {
+    val keyForUsername = stringPreferencesKey(getKeyForUsername(service))
+    val keyForPassword = stringPreferencesKey(getKeyForPassword(service))
+    val keyForCipherStorage = stringPreferencesKey(getKeyForCipherStorage(service))
+    callSuspendable {
+      prefs.edit {
+        it[keyForUsername] = Base64.encodeToString(encryptionResult.username, Base64.DEFAULT)
+        it[keyForPassword] = Base64.encodeToString(encryptionResult.password, Base64.DEFAULT)
+        it[keyForCipherStorage] = encryptionResult.cipherName
+      }
+    }
+  }
+
+  override fun getUsedCipherNames(): Set<String?> {
+    val result: MutableSet<String?> = HashSet()
+    val keys = prefsData.asMap().keys.map { it.name }
+    for (key in keys) {
+      if (isKeyForCipherStorage(key)) {
+        val cipher = prefsData[stringPreferencesKey(key)]
+        result.add(cipher)
+      }
+    }
+    return result
+  }
+
+  private fun <T> callSuspendable(block: suspend () -> T): T {
+    return runBlocking {
+      block()
+    }
+  }
+
+  private fun getBytesForUsername(service: String): ByteArray? {
+    val key = stringPreferencesKey(getKeyForUsername(service))
+    return getBytes(key)
+  }
+
+  private fun getBytesForPassword(service: String): ByteArray? {
+    val key = stringPreferencesKey(getKeyForPassword(service))
+    return getBytes(key)
+  }
+
+  private fun getCipherStorageName(service: String): String? {
+    val key = stringPreferencesKey(getKeyForCipherStorage(service))
+    return prefsData[key]
+  }
+
+  private fun getBytes(prefKey: Preferences.Key<String>): ByteArray? {
+    return prefsData[prefKey]?.let { Base64.decode(it, Base64.DEFAULT) }
+  }
+}
